e23aab90946ed27e11eb0a7b48abacbbe84602d8
pixelpeter <info@pixelpeter.de>
2019-03-15 20:28:08 +0100

    ADD: use travis-ci to test against multiple versions of Laravel

---
+++ build/55/build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+VERSION=55
+
+# save original composer json
+cp ./composer.json ./original-composer.json
+
+# save original phpunit.xml
+cp ./phpunit.xml ./original-phpunit.xml
+
+# use laravel specific version of composer.json
+cp ./build/$VERSION/composer.json ./composer.json
+
+# use laravel specific version of phpunit.xml
+cp ./build/$VERSION/phpunit.xml ./phpunit.xml
+
+# install laravel specific dependencies
+composer update --prefer-source --no-interaction
+
+mkdir -p ./build/logs
+php ./vendor/bin/phpunit --configuration ./phpunit.xml --coverage-text --coverage-clover ./build/logs/clover.xml
+
+# restore original composer.json
+rm ./composer.json
+mv ./original-composer.json ./composer.json
+
+# restore original phpunit.xml
+rm ./phpunit.xml
+mv ./original-phpunit.xml ./phpunit.xml
